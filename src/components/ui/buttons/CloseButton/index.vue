afd91df32a3225ee332fd54920c9387513f54fb1
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:07:20 +0300

    chore: added components close and sort button

---
+++ src/components/ui/buttons/CloseButton/index.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'CloseButton',
+};
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    tag: {
+        type: String,
+        default: 'button',
+    },
+});
+
+const root = ref(null);
+</script>
+
+<template>
+    <component
+        :is="props.tag"
+        ref="root"
+        type="button"
+        class="button-close"
+    >
+        X
+    </component>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
---
+++ src/components/ui/buttons/SortButton/index.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'SortButton',
+};
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['sort-items']);
+
+const props = defineProps({
+    tag: {
+        type: String,
+        default: 'button',
+    },
+});
+const root = ref(null);
+</script>
+
+<template>
+    <component
+        :is="props.tag"
+        ref="root"
+        type="button"
+        class="button-sort"
+        @click="emit('sort-items')"
+    >
+        Сортировка по количеству репозиториев
+    </component>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
