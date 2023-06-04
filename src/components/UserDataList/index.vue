fb1da335d0c72301d84540a49deeab8bfbb3178e
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:10:43 +0300

    chore: added component user description

---
+++ src/components/UserDataList/index.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'UserDataList',
+};
+</script>
+
+<script setup>
+const props = defineProps({
+    usersData: {
+        type: Array,
+        default: () => [],
+    },
+    title: {
+        type: String,
+        default: '',
+    },
+});
+</script>
+
+<template>
+    <div v-if="props.usersData.length" class="user-block">
+        <h3 class="user-block-title">
+            {{ props.title }}
+        </h3>
+        <ul class="user-list">
+            <li v-for="item in props.usersData" :key="item.id" class="user-list-item ">
+                <a :href="item.html_url" target="_blank" class="user-list-link">{{ item.login ? item.login : item.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
