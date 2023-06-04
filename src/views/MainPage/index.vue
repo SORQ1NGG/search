0efc7f028e17deb8d8fed1bec1d8afbe1378232b
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:26:36 +0300

    chore: added view component main page

---
+++ src/views/MainPage/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'MainPage',
+};
+</script>
+
+<script setup>
+import MainSection from '@/components/MainSectionUsers/index.vue';
+</script>
+
+<template>
+    <div class="main-page">
+        <div class="main-page__wrapper content-container">
+            <MainSection />
+        </div>
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
