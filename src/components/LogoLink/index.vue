4faed3bf4ab9afec7d0caf51ffe2c9fbd9405ba7
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:20:24 +0300

    chore: logo for header

---
+++ src/components/LogoLink/index.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'LogoLink',
+};
+</script>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { ROUTE_NAMES } from '@/constants/router.js';
+
+const router = useRouter();
+</script>
+
+<template>
+    <div class="logo" @click="router.push({ name: ROUTE_NAMES.MAIN })">
+        Git
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
