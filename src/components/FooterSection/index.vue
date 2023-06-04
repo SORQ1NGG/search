4147c68c3511361365467ee6a37287763a75a4e1
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:20:03 +0300

    chore: added footer and header

---
+++ src/components/FooterSection/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'FooterSection',
+};
+</script>
+
+<template>
+    <footer class="footer">
+        <div class="footer__inner">
+            Search
+        </div>
+    </footer>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
---
+++ src/components/HeaderSection/index.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'HeaderSection',
+};
+</script>
+
+<script setup>
+import LogoLink from '@/components/LogoLink/index.vue';
+</script>
+
+<template>
+    <header
+        class="header"
+    >
+        <div class="header__inner">
+            <div class="content-container header__content">
+                <LogoLink />
+            </div>
+        </div>
+    </header>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
