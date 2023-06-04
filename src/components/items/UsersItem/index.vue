1d7aa9569f651a7f042ca4c36379ab3e11a41050
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:09:22 +0300

    chore: added components with users

---
+++ src/components/items/UsersItem/index.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'UsersItem',
+};
+</script>
+
+<script setup>
+const props = defineProps({
+    login: {
+        type: [String, Object],
+        default: '',
+    },
+    avatar: {
+        type: [String, Object],
+        default: '',
+    },
+});
+</script>
+
+<template>
+    <div class="users__item">
+        <img
+            class="users__item-img"
+            :src="`${props.avatar}`"
+            alt="img"
+        >
+        <span class="users__item-name">{{ props.login }}</span>
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
