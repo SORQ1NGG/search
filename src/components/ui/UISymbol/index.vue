bb30a6f3d80e153cd8b1d0815e82fd476f9c4c37
SORQ1NGG <daniil.sork@gmail.com>
2023-06-05 01:17:35 +0300

    chore: added ui symbol component for icon

---
+++ src/components/ui/UISymbol/index.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'UISymbol',
+};
+</script>
+
+<script setup>
+import { computed } from 'vue';
+import iconSort from '@/assets/icons/symbols/sort.svg';
+
+const iconPath = computed(() => {
+    if (!iconSort) return '';
+
+    let icon = null;
+
+    try {
+        icon = iconSort;
+    } catch (e) {
+        console.error(iconSort, e);
+        return '';
+    }
+
+    return icon;
+});
+
+</script>
+
+<template>
+    <svg
+        v-if="iconPath"
+        class="symbol"
+    >
+        <use :xlink:href="`#${iconPath}`" />
+    </svg>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
