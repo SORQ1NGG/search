7fa2151f1441b2d36f239240dcf2bd2396b3bfd3
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:21:19 +0300

    chore: added component user description info(repos, followers and following)

---
+++ src/components/items/UserDescriptionItem/index.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: 'UserDescriptionItem',
+};
+</script>
+
+<script setup>
+import UsersList from '@/components/UserDataList/index.vue';
+
+const props = defineProps({
+    userData: {
+        type: [String, Object, Array],
+        default: '',
+    },
+    userRepos: {
+        type: Array,
+        default: () => [],
+    },
+    userFollowers: {
+        type: Array,
+        default: () => [],
+    },
+    userFollowing: {
+        type: Array,
+        default: () => [],
+    },
+    loadingImg: {
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <div v-if="props.userData.avatar_url && props.userData.login" class="user">
+        <img :src="props.userData.avatar_url" alt="img">
+        <h2 class="user-name">
+            {{ props.userData.login }}
+        </h2>
+        <UsersList :users-data="props.userFollowing" :title="'Following'" />
+        <UsersList :users-data="props.userFollowers" :title="'Followers'" />
+        <UsersList :users-data="props.userRepos" :title="'Repositories'" />
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
