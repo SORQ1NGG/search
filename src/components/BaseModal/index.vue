4753ce86417449ca33b581628da2ee8250767b81
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:06:06 +0300

    chore: added components modal for user info

---
+++ src/components/BaseModal/index.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    name: 'BaseModal',
+};
+</script>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+import CloseButton from '@/components/ui/buttons/CloseButton/index.vue';
+
+const emit = defineEmits(['close']);
+
+defineProps({
+    isOpen: {
+        type: Boolean,
+    },
+});
+const root = ref(null);
+
+const isOpened = ref(false);
+
+const toggleOpened = force => {
+    isOpened.value = force ?? !isOpened.value;
+};
+
+const close = () => {
+    if (!isOpened.value) return;
+
+    emit('close');
+};
+
+onMounted(() => {
+    toggleOpened(true);
+    root.value.focus();
+});
+
+onUnmounted(() => {
+    toggleOpened(false);
+});
+</script>
+
+<template>
+    <transition name="fade">
+        <div
+            ref="root"
+            class="modal-mask"
+            tabindex="0"
+            @keydown.esc="close"
+        >
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <div class="modal-close">
+                        <CloseButton @click="close" />
+                    </div>
+                    <div class="modal-body">
+                        <slot />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
