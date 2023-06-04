cc62b1167341400d5ddefaf88c7d035c3167d782
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:19:40 +0300

    chore: added a field for searching users by login

---
+++ src/components/items/SearchItems/index.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'SearchItems',
+};
+</script>
+
+<script setup>
+import { ref } from 'vue';
+import BaseInput from '@/components/BaseInput/index.vue';
+
+const searchData = ref('');
+
+const emit = defineEmits(['search']);
+
+</script>
+
+<template>
+    <BaseInput
+        v-model="searchData"
+        label-text="Git"
+        with-icon
+        type="text"
+        type-modifiers="search"
+        :placeholder="'Search login...'"
+        @keyup="emit('search', searchData)"
+    />
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
