147b0758075442e182eb77a1382c18262592fb39
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 15:23:28 +0300

    chore: added the number of users when searching

---
+++ src/components/ui/CounterMessage/index.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'CounterMessage',
+};
+</script>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    usersCount: {
+        type: [Array, Number],
+        default: null,
+    },
+});
+
+const counterMessage = computed(() => {
+    return (props.usersCount > 0)
+        ? `Найдено ${props.usersCount} пользователей`
+        : (props.usersCount === 0)
+            ? 'По вашему запросу пользователей не найдено'
+            : null;
+    // if (props.usersCount > 0) {
+    //     return `Найдено ${props.usersCount} пользователей`;
+    // } else if (props.usersCount === 0) {
+    //     return 'По вашему запросу пользователей не найдено';
+    // }
+});
+</script>
+
+<template>
+    <div>
+        {{ counterMessage }}
+    </div>
+</template>
