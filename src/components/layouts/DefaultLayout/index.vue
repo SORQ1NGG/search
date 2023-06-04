55f8e079ec14242f2a287ef3a04a13c1fb330df0
SORQ1NGG <daniil.sork@gmail.com>
2023-06-04 17:28:33 +0300

    chore: added header and footer in layout

--- src/components/layouts/DefaultLayout/index.vue
+++ src/components/layouts/DefaultLayout/index.vue
@@ -4,6 +4,13 @@ export default {
 };
 </script>
 
+<script setup>
+import HeaderSection from '@/components/HeaderSection/index.vue';
+import FooterSection from '@/components/FooterSection/index.vue';
+</script>
+
 <template>
+    <HeaderSection />
     <slot />
+    <FooterSection />
 </template>
